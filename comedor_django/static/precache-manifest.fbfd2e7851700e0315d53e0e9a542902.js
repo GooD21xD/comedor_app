383b79262612cb1a2ebd584ec065057e6c6ca1be
carlos <gm.cg.05@gmail.com>
2023-08-25 19:04:51 -0500

    Cambiando el url react

---
+++ comedor_django/static/precache-manifest.fbfd2e7851700e0315d53e0e9a542902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2492c6c3d5f820d35ca",
+    "url": "/static/css/main.16aef5d8.chunk.css"
+  },
+  {
+    "revision": "a2492c6c3d5f820d35ca",
+    "url": "/static/js/main.a2492c6c.chunk.js"
+  },
+  {
+    "revision": "7940696f92c4ec83dcff",
+    "url": "/static/js/1.7940696f.chunk.js"
+  },
+  {
+    "revision": "eddfae78ee912f471142",
+    "url": "/static/js/2.eddfae78.chunk.js"
+  },
+  {
+    "revision": "516ec4095d5ef8b1b3ce",
+    "url": "/static/js/runtime~main.516ec409.js"
+  },
+  {
+    "revision": "63383907ac01b14cf0c8a06872673c8d",
+    "url": "/index.html"
+  }
+];
--- comedor_django/static/service-worker.js
+++ comedor_django/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c0432f8826ae9bc69ed8273fa593465.js"
+  "/precache-manifest.fbfd2e7851700e0315d53e0e9a542902.js"
 );
 
 workbox.clientsClaim();
